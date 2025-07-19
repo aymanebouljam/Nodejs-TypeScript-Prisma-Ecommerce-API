680596f716a1036cc0897a4e8902fd720ba87f57
Aymane Bouljam <its.aymane.bouljam@gmail.com>
2025-07-19 20:55:25 +0100

    Validate signup data using zod

--- backend/src/exceptions/root.ts
+++ backend/src/exceptions/root.ts
@@ -2,6 +2,7 @@ export enum ErrorCode {
   USER_NOT_FOUND = 1001,
   USER_ALREADY_EXISTS = 1002,
   INVALID_CRENDETIALS = 1003,
+  Unprocessable_Entity = 1004,
 }
 
 export class HttpException extends Error {
