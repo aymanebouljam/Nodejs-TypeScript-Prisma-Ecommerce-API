ae8f9f95a80c62a777d94a3eb59ef797956b44e3
Aymane Bouljam <196711990+aymanebouljam@users.noreply.github.com>
2025-07-19 19:38:46 +0100

    Add base Node.js API structure

---
+++ backend/src/exceptions/root.ts
@@ -0,0 +1,25 @@
+export enum ErrorCode {
+  USER_NOT_FOUND = 1001,
+  USER_ALREADY_EXISTS = 1002,
+  INVALID_CRENDETIALS = 1003,
+}
+
+export class HttpException extends Error {
+  message: string;
+  errorCode: ErrorCode;
+  statusCode: number;
+  errors: any;
+
+  constructor(
+    message: string,
+    errorCode: ErrorCode,
+    statusCode: number,
+    errors: any
+  ) {
+    super(message);
+    this.message = message;
+    this.errorCode = errorCode;
+    this.statusCode = statusCode;
+    this.errors = errors;
+  }
+}
